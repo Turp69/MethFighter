45416ba8df545d49a0c3ac240967d0e9119b1d13
Turp69 <150671706+Turp69@users.noreply.github.com>
2024-01-03 17:56:14 +0700

    Herrow

---
+++ src/main.js
@@ -0,0 +1,22 @@
+import Phaser from 'phaser'
+import MathFighterScene from'../src/scenes/MathFighterScene'
+
+const config = {
+	type: Phaser.AUTO,
+	parent: 'app',
+	width: 480,
+	height: 640,
+	physics: {
+		default: 'arcade',
+		arcade: {
+			gravity: { y: 200 },
+		},
+	},
+	scene: [MathFighterScene],
+	scale: {
+		mode: Phaser.Scale.FIT,
+		autoCenter: Phaser.Scale.CENTER_BOTH
+	}
+}
+
+export default new Phaser.Game(config)
